2aa26dd23a850a78c419c4aebfa1caf1899d9eba
hareland <packagist@proton.me>
2025-11-10 17:10:04 +0100

    chore: simplify eslint configuration by removing unused dirs and files settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,11 @@
 import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
 
 export default createConfigForNuxt({
-  dirs: {
-    src: [
-      './layer',
-      './docs',
-    ],
-  },
   features: {
     tooling: true,
     stylistic: true,
   },
 }, {
-  files: [
-    'cli/**/*.ts',
-  ],
 }).append(
   {
     rules: {
