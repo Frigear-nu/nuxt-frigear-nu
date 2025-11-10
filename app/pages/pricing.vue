3ba9a0c17ea5297459e6f143cfa15dad6d76ff28
hareland <packagist@proton.me>
2025-11-10 17:10:23 +0100

    chore: enhance pricing page layout with improved structure and footer socials data integration

--- app/pages/pricing.vue
+++ app/pages/pricing.vue
@@ -1,19 +1,34 @@
+<script lang="ts" setup>
+// use shared data available in all components:
+const { data: footer } = await useSiteFooter()
+// const { data: header } = await useSiteHeader()
+</script>
+
 <template>
-  <UEmpty
-      icon="i-lucide-info"
-      title="Custom page"
-      :actions="[
-      {
-        icon: 'i-lucide-home',
-        label: 'Back home',
-        color: 'neutral',
-        variant: 'subtle',
-        to: '/'
-      }
-    ]"
-  >
-    <template #description>
-      This page can be edited in <code>app/pages/pricing.vue</code>.
-    </template>
-  </UEmpty>
+  <UPageBody>
+    <UContainer class="flex  flex-col gap-4">
+      <UEmpty
+        icon="i-lucide-info"
+        title="Custom page"
+        :actions="[
+          {
+            icon: 'i-lucide-home',
+            label: 'Back home',
+            color: 'neutral',
+            variant: 'subtle',
+            to: '/',
+          },
+        ]"
+      >
+        <template #description>
+          This page can be edited in <code>app/pages/pricing.vue</code>.
+        </template>
+      </UEmpty>
+      <UCard>
+        There is some more shit you can do:
+        e.g get all socials that can be changed in: <code class="border border-muted rounded p-0.5">content/footer.yml</code>
+        <pre>{{ footer && footer.socials }}</pre>
+      </UCard>
+    </UContainer>
+  </UPageBody>
 </template>
