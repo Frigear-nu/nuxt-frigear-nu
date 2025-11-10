ff65f61977dd730b2c851b369047b3722ae6850d
hareland <packagist@proton.me>
2025-11-10 15:48:22 +0100

    chore: update project configuration and wrangler settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,20 @@
 export default defineNuxtConfig({
-  modules: ['nuxt-studio'],
-  nitro: {
-    cloudflare: {
-      nodeCompat: true,
-      deployConfig: true,
+    modules: ['nuxt-studio'],
+    nitro: {
+        cloudflare: {
+            nodeCompat: true,
+            deployConfig: true,
+        },
     },
-  },
     site: {
-      name: 'Frigear.nu',
+        name: 'Frigear.nu',
     },
-  studio: {
-    repository: {
-      provider: 'github',
-      owner: 'hareland',
-      repo: 'hareland-eu',
-      branch: process.env.STUDIO_GITHUB_BRANCH_NAME || 'main',
+    studio: {
+        repository: {
+            provider: 'github',
+            owner: 'hareland',
+            repo: 'hareland-eu',
+            branch: process.env.STUDIO_GITHUB_BRANCH_NAME || 'main',
+        },
     },
-  },
 })
