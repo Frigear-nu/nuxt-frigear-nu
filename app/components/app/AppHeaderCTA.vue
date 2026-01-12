e5a800eb318d65fb45d95e308e12759ce615c833
hareland <packagist@proton.me>
2026-01-12 20:47:57 +0100

    feat: add Auth BTN in CTA

---
+++ app/components/app/AppHeaderCTA.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { useSiteI18n } from '#imports'
+
+const { t } = useSiteI18n()
+</script>
+
+<template>
+  <UButton
+    :label="t('Sign In') || 'Logg Ind'"
+    to="/sign-in"
+    trailing-icon="i-lucide-lock"
+  />
+</template>
