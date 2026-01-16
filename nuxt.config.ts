f042f9e8aa41c79328ef19cd220ebe29c3d11df9
hareland <packagist@proton.me>
2026-01-16 16:35:00 +0100

    feat: add JWT secret to runtimeConfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,6 +17,7 @@ export default defineNuxtConfig({
     preference: 'dark',
   },
   runtimeConfig: {
+    jwtSecret: 'some-string-longer-than-32-chars-to-issue-jwt',
     stripeWebhookSecret: '',
   },
   routeRules: {
