8f9b852c8c964c4592ce84a7ad542d24828bd37b
hareland <packagist@proton.me>
2026-01-16 18:15:47 +0100

    refactor: update `findUserByEmail` to return `null` if no user is found

--- server/utils/user.ts
+++ server/utils/user.ts
@@ -1,6 +1,6 @@
 import type { Users } from 'hub:db:schema'
 
-export const findUserByEmail = (email: string): Promise<Users> => {
+export const findUserByEmail = (email: string): Promise<Users | null> => {
   // @ts-expect-error There is some typing issue with drizzle for now
   return db.query.users.findFirst({ where: { email } })
 }
