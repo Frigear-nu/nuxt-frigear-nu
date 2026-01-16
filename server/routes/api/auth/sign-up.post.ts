82ae3547a8f89b71276973c17324c783192190ce
hareland <packagist@proton.me>
2026-01-16 03:36:30 +0100

    feat: update sign-up route to use `findUserByEmail` and add Supabase user migration logic

--- server/routes/api/auth/sign-up.post.ts
+++ server/routes/api/auth/sign-up.post.ts
@@ -9,20 +9,28 @@ export default defineEventHandler(async (event) => {
   const { name, email, password, redirect } = await useValidatedBody(event, signUpSchema)
 
   // check if we need to migrate this user:
-  // @ts-expect-error There are soem typing issue with drizzle for now
-  const internalUser = await db.query.users.findFirst({ where: { email } })
+  const internalUser = await findUserByEmail(email)
 
   if (internalUser) throw createError({ statusCode: 400, message: 'User already exists.' })
 
   // todo: migrate the user coming from supabase somehow but via email link.
   const sb = serverSupabaseServiceRole(event)
-  const { data: sbUser } = await sb.from('users').select('*').eq('email', email).maybeSingle()
-  const createdUser = await db.insert(schema.users).values({
+  const { data: sbUser } = await sb.from('users')
+    .select('*')
+    .eq('email', email)
+    .maybeSingle<{ id: string }>()
+
+  //
+  let [createdUser] = await db.insert(schema.users).values({
     name,
     email,
     password: await hashPassword(password),
-    isMigrated: !sbUser,
   }).returning()
+
+  if (sbUser) {
+    await migrateSupabaseAccountById(createdUser.id, sbUser.id)
+    createdUser = await findUserByEmail(createdUser.email)
+  }
 
   // todo: some mapping probably needs to be done here.
   await setUserSession(event, { user: createdUser })
