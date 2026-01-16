bfad201e15c0a0dca48f7bb5ee46b1943d697e50
hareland <packagist@proton.me>
2026-01-16 21:24:31 +0100

    refactor: skip auth middleware for non-account routes

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,6 +1,9 @@
 import { useUserSession } from '#imports'
 
 export default defineNuxtRouteMiddleware((to) => {
+  if (!to.path.startsWith('/account')) {
+    return
+  }
   const { loggedIn } = useUserSession()
 
   if (loggedIn.value) return
