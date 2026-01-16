b480826e57f673ef280bd7f9a6d0a93b6c1fa965
hareland <packagist@proton.me>
2026-01-16 02:57:56 +0100

    feat: implement user sign-up route with validation and session handling

--- server/routes/api/auth/sign-up.post.ts
+++ server/routes/api/auth/sign-up.post.ts
@@ -1,3 +1,20 @@
-export default defineEventHandler(() => {
-  throw createError('Not yet implemented.')
+import { signUpSchema } from '#shared/auth'
+import { useValidatedBody } from 'h3-zod'
+
+export default defineEventHandler(async (event) => {
+  const { user } = await getUserSession(event)
+  if (!user) return sendRedirect(event, '/account')
+
+  const { name, email, password, redirect } = await useValidatedBody(event, signUpSchema)
+  const createdUser = await db.insert(schema.users).values({
+    name,
+    email,
+    password: await hashPassword(password),
+    isMigrated: true,
+  }).returning()
+
+  // todo: some mapping probably needs to be done here.
+  await setUserSession(event, { user: createdUser })
+
+  return sendRedirect(event, redirect || '/account')
 })
