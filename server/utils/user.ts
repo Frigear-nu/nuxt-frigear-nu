ae003aa8f84c75355b06aec55949e7dad717d2d7
hareland <packagist@proton.me>
2026-01-16 21:24:40 +0100

    refactor: use `eq` from `drizzle-orm` in `findUserByEmail` for improved query handling

--- server/utils/user.ts
+++ server/utils/user.ts
@@ -1,6 +1,7 @@
 import type { Users } from 'hub:db:schema'
+import { eq } from 'drizzle-orm'
 
 export const findUserByEmail = (email: string): Promise<Users | null> => {
   // @ts-expect-error There is some typing issue with drizzle for now
-  return db.query.users.findFirst({ where: { email } })
+  return db.query.users.findFirst({ where: eq(schema.users.email, email) })
 }
