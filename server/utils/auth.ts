c9facf6655db0dcad85983aac5d0e6de16646f99
hareland <packagist@proton.me>
2026-01-16 18:15:59 +0100

    feat: add `createSafeId` and `authenticateUser` utilities for user session handling and JWT creation

---
+++ server/utils/auth.ts
@@ -0,0 +1,18 @@
+import { createId } from '@paralleldrive/cuid2'
+import type { Users } from 'hub:db:schema'
+import type { H3Event } from 'h3'
+import { encodeJwt } from '@nitrotool/jwt'
+
+export const createSafeId = () => createId()
+
+export const authenticateUser = async (event: H3Event, user: Users, redirect?: string) => {
+  await setUserSession(event, {
+    user: {
+      ...user,
+      jwt: await encodeJwt(user),
+    },
+    loggedInAt: Date.now(),
+  })
+
+  return sendRedirect(event, redirect || '/')
+}
