20061c06e99865c2f5ae13e19bcb659fc020cddc
hareland <packagist@proton.me>
2026-01-16 03:11:43 +0100

    feat: enhance sign-up route with user existence check and migration support

--- server/routes/api/auth/sign-up.post.ts
+++ server/routes/api/auth/sign-up.post.ts
@@ -1,16 +1,27 @@
 import { signUpSchema } from '#shared/schema/auth'
 import { useValidatedBody } from 'h3-zod'
+import { serverSupabaseServiceRole } from '#supabase/server'
 
 export default defineEventHandler(async (event) => {
   const { user } = await getUserSession(event)
-  if (!user) return sendRedirect(event, '/account')
+  if (user) return sendRedirect(event, '/account')
 
   const { name, email, password, redirect } = await useValidatedBody(event, signUpSchema)
+
+  // check if we need to migrate this user:
+  // @ts-expect-error There are soem typing issue with drizzle for now
+  const internalUser = await db.query.users.findFirst({ where: { email } })
+
+  if (internalUser) throw createError({ statusCode: 400, message: 'User already exists.' })
+
+  // todo: migrate the user coming from supabase somehow but via email link.
+  const sb = serverSupabaseServiceRole(event)
+  const { data: sbUser } = await sb.from('users').select('*').eq('email', email).maybeSingle()
   const createdUser = await db.insert(schema.users).values({
     name,
     email,
     password: await hashPassword(password),
-    isMigrated: true,
+    isMigrated: !sbUser,
   }).returning()
 
   // todo: some mapping probably needs to be done here.
