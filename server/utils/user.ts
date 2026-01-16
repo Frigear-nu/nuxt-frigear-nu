782587c931e23777dc943d406236a8ef21b04790
hareland <packagist@proton.me>
2026-01-16 03:36:09 +0100

    feat: add `findUserByEmail` utility to query users by email

---
+++ server/utils/user.ts
@@ -0,0 +1,6 @@
+import type { Users } from 'hub:db:schema'
+
+export const findUserByEmail = (email: string): Promise<Users> => {
+  // @ts-expect-error There is some typing issue with drizzle for now
+  return db.query.users.findFirst({ where: { email } })
+}
