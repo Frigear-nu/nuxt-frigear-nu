3e19216872810fac02db987ef42c6ff9c0ffb0a1
hareland <packagist@proton.me>
2026-01-16 16:35:18 +0100

    feat: define comprehensive entity relations in database schema

--- server/db/schema/relations.ts
+++ server/db/schema/relations.ts
@@ -1,16 +1,53 @@
+import { relations } from 'drizzle-orm'
 import * as stripe from './stripe'
 import * as user from './user'
 
-import { relations } from 'drizzle-orm'
-
+// User relations
 export const usersRelations = relations(user.users, ({ many }) => ({
   stripeCustomers: many(stripe.stripeCustomers),
+  magicLinks: many(user.magicLinks),
+  passwordResets: many(user.passwordResets),
+  oauthApps: many(user.oauthApps),
+  passkeys: many(user.passkeys),
 }))
 
-export const stripeUsersRelations = relations(stripe.stripeCustomers, ({ one, many }) => ({
+export const magicLinksRelations = relations(user.magicLinks, ({ one }) => ({
   user: one(user.users, {
-    fields: [stripe.stripeCustomers.userId],
+    fields: [user.magicLinks.userId],
     references: [user.users.id],
+  }),
+}))
+
+export const passwordResetsRelations = relations(user.passwordResets, ({ one }) => ({
+  user: one(user.users, {
+    fields: [user.passwordResets.userId],
+    references: [user.users.id],
+  }),
+}))
+
+export const oauthAppsRelations = relations(user.oauthApps, ({ one }) => ({
+  user: one(user.users, {
+    fields: [user.oauthApps.userId],
+    references: [user.users.id],
+  }),
+}))
+
+export const passkeysRelations = relations(user.passkeys, ({ one }) => ({
+  user: one(user.users, {
+    fields: [user.passkeys.userId],
+    references: [user.users.id],
+  }),
+}))
+
+// Stripe relations
+export const stripeProductsRelations = relations(stripe.stripeProducts, ({ many }) => ({
+  prices: many(stripe.stripePrices),
+}))
+
+export const stripePricesRelations = relations(stripe.stripePrices, ({ one, many }) => ({
+  product: one(stripe.stripeProducts, {
+    fields: [stripe.stripePrices.productId],
+    references: [stripe.stripeProducts.id],
   }),
   subscriptions: many(stripe.stripeSubscriptions),
 }))
@@ -20,4 +57,16 @@ export const stripeSubscriptionsRelations = relations(stripe.stripeSubscriptions
     fields: [stripe.stripeSubscriptions.userId],
     references: [stripe.stripeCustomers.id],
   }),
+  price: one(stripe.stripePrices, {
+    fields: [stripe.stripeSubscriptions.priceId],
+    references: [stripe.stripePrices.id],
+  }),
+}))
+
+export const stripeCustomersRelations = relations(stripe.stripeCustomers, ({ one, many }) => ({
+  user: one(user.users, {
+    fields: [stripe.stripeCustomers.userId],
+    references: [user.users.id],
+  }),
+  subscriptions: many(stripe.stripeSubscriptions),
 }))
