6ac403291070b01a31f8e6e1b2b31fe46cc8a03c
hareland <packagist@proton.me>
2026-01-16 16:35:46 +0100

    refactor: update sign-up error message and simplify user creation logic

--- server/routes/api/auth/sign-up.post.ts
+++ server/routes/api/auth/sign-up.post.ts
@@ -11,22 +11,16 @@ export default defineEventHandler(async (event) => {
   // check if we need to migrate this user:
   const internalUser = await findUserByEmail(email)
 
-  if (internalUser) throw createError({ statusCode: 400, message: 'User already exists.' })
+  if (internalUser) throw createError({ statusCode: 409, message: 'error.exists' })
 
   //
-  let [createdUser] = await db.insert(schema.users).values({
+  const [createdUser] = await db.insert(schema.users).values({
     name,
     email,
     password: await hashPassword(password),
   }).returning()
 
-  const serviceRole = serverSupabaseServiceRole(event)
-  const sbUser = await findSupabaseUserByEmail(serviceRole, email)
-  if (sbUser) {
-    await migrateSupabaseAccountById(serviceRole, createdUser.id, sbUser.id)
-    createdUser = await findUserByEmail(createdUser.email)
-    if (!createdUser.isMigrated) throw new Error('Could not migrate supabase account.')
-  }
+  // check if email verification is enabled.
 
   // todo: some mapping probably needs to be done here.
   await setUserSession(event, { user: createdUser })
