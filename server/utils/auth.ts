5528135cadfa3bc1ff5c0d7dde7f783bfb9960c4
hareland <packagist@proton.me>
2026-01-17 17:03:00 +0100

    refactor: replace `encodeJwtRaw` with `encodeJwt` in `authenticateUser` for cleaner usage

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -1,16 +1,15 @@
 import { createId } from '@paralleldrive/cuid2'
 import type { Users } from 'hub:db:schema'
 import type { H3Event } from 'h3'
-import { encodeJwtRaw } from '@nitrotool/jwt/core'
+import { encodeJwt } from '@nitrotool/jwt'
 
 export const createSafeId = () => createId()
 
 export const authenticateUser = async (event: H3Event, user: Users, redirect?: string) => {
-  const secret = useRuntimeConfig(event).jwtSecret
   await setUserSession(event, {
     user: {
       ...user,
-      jwt: await encodeJwtRaw(user, secret),
+      jwt: await encodeJwt(user),
     },
     loggedInAt: Date.now(),
   })
