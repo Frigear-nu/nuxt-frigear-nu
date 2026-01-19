434578d1c62b8a5828a5d18fa800ea07f305b04d
hareland <packagist@proton.me>
2026-01-19 03:14:13 +0100

    feat: add utility function for sending emails using Resend API

---
+++ server/utils/email.ts
@@ -0,0 +1,37 @@
+import { Resend } from 'resend'
+import type { H3Event } from 'h3'
+
+type Email = {
+  to: string
+  from: string
+  subject: string
+  replyTo?: string
+  html: string
+}
+
+export const sendEmail = async (event: H3Event, message: Email) => {
+  const { resend: { apiKey } } = useRuntimeConfig(event)
+
+  if (!apiKey) {
+    throw createError({ statusCode: 500, message: 'Missing API-Key.' })
+  }
+
+  if (!message.from || !message.to) {
+    throw createError({ statusCode: 500, message: 'Missing from or to address.' })
+  }
+
+  const resend = new Resend(apiKey)
+
+  const { error } = await resend.emails.send({
+    from: message.from,
+    to: [message.to],
+    subject: message.subject,
+    html: message.html,
+    // Resend’s Node SDK uses replyTo (camelCase) - but the API expects reply_to (snake_case) and it works. - DONT CHANGE
+    reply_to: message.replyTo,
+  })
+
+  if (error) {
+    throw createError({ statusCode: 502, message: error.message ?? 'Error sending email' })
+  }
+}
