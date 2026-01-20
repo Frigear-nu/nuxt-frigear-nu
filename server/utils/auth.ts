659d5e080900f8a7c7531ea26323da406cd4043a
hareland <packagist@proton.me>
2026-01-20 01:23:15 +0100

    refactor: extract `mapUserToSession` and `mapUserToJwt` utility functions in `auth.ts` for cleaner session and JWT mapping

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -1,22 +1,31 @@
 import { createId } from '@paralleldrive/cuid2'
 import type { Users } from 'hub:db:schema'
 import type { H3Event } from 'h3'
+import type { ExtendableJwtPayload } from '@nitrotool/jwt/core'
 
 export const createSafeId = () => createId()
 
-// todo: investigate usage of function,
-//  if it would be easier to use JWT in the session?
 export const authenticateUser = async (event: H3Event, user: Users, redirect?: string) => {
   await setUserSession(event, {
-    // todo: decide on structure here:
-    //  - Might be enough with only a few details, e.g id,name,email
-    user: {
-      ...user,
-      jwt: await encodeJwt(user),
-      emailVerified: !!user?.emailVerifiedAt,
-    },
+    user: await mapUserToSession(user),
     loggedInAt: Date.now(),
   })
 
   return sendRedirect(event, redirect || '/')
 }
+
+export const mapUserToSession = async (user: Users) => {
+  return ({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    emailVerifiedAt: user.emailVerifiedAt,
+    avatarUrl: user.avatarUrl,
+    // todo: maybe issue JWT here?
+  })
+}
+
+export const mapUserToJwt = (user: Users): ExtendableJwtPayload => ({
+  sub: `${user.id}`,
+// todo: investigate what we need here.
+})
