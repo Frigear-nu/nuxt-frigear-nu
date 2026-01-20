d7175d67c5c6adc6cdf232aa260ad5951d053091
hareland <packagist@proton.me>
2026-01-20 02:53:16 +0100

    feat: update `export-users.sql` to include migration and verification fields

--- supabase/export/export-users.sql
+++ supabase/export/export-users.sql
@@ -1,11 +1,5 @@
 -- This SQL exports all users with some more details from Supabase.
 select
-    u.id as supabase_id,
-    coalesce(i.provider, 'email') as supabase_provider,
-    coalesce(
-            i.identity_data ->> 'email',
-            u.email
-    ) as email,
     coalesce(
             i.identity_data ->> 'full_name',
             i.identity_data ->> 'name',
@@ -13,12 +7,21 @@ select
             u.raw_user_meta_data ->> 'name'
     ) as name,
     coalesce(
+            i.identity_data ->> 'email',
+            u.email
+    ) as email,
+    c.stripe_customer_id as password_hash,
+    coalesce(
             i.identity_data ->> 'avatar_url',
             i.identity_data ->> 'picture',
             u.raw_user_meta_data ->> 'avatar_url'
     ) as avatar_url,
-    c.stripe_customer_id,
-    u.created_at
+    0 as is_migrated,
+    u.id as supabase_id,
+    null as last_login_at,
+    null as email_verified_at,
+    extract(epoch from u.created_at)::int as created_at,
+    coalesce(i.provider, 'email') as supabase_provider
 from auth.users u
          left join lateral (
     select *
@@ -31,14 +34,14 @@ from auth.users u
             when 'apple' then 3
             else 99
             end
-    limit 1
+        limit 1
     ) i on true
-         left join (
+    left join (
     select
-        id,
-        max(stripe_customer_id) as stripe_customer_id
+    id,
+    max(stripe_customer_id) as stripe_customer_id
     from public.customers
     group by id
     having count(*) = 1
-) c on c.id = u.id
+    ) c on c.id = u.id
 order by u.created_at;
