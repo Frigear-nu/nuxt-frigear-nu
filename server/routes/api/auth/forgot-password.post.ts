a03587994c20148536a0665fe0e172b491e1f7a6
hareland <packagist@proton.me>
2026-01-21 05:11:56 +0100

    refactor: implement forgot password endpoint with validation, token generation, and email support

--- server/routes/api/auth/forgot-password.post.ts
+++ server/routes/api/auth/forgot-password.post.ts
@@ -1,3 +1,54 @@
-export default defineEventHandler(() => {
-  throw createError('Not yet implemented.')
+import { useValidatedBody } from 'h3-zod'
+import { forgotPasswordSchema } from '#shared/schema/auth'
+import { ServerError } from '@nitrotool/errors'
+import { addMinutes } from 'date-fns'
+import PasswordResetEmail from '#shared/emails/auth/PasswordResetEmail.vue'
+import { withQuery } from 'ufo'
+
+export default defineEventHandler(async (event) => {
+  const { mail: { from, to: replyTo } } = useRuntimeConfig(event)
+  const { email } = await useValidatedBody(event, forgotPasswordSchema)
+
+  const user = await findUserByEmail(email)
+
+  if (!user) {
+    // We don't want to let any attacker know this is not a valid email.
+    return { ok: true }
+  }
+
+  const token = createSafeId()
+  const expiresAt = addMinutes(new Date(), 30)
+  const [createdReset] = await db.insert(schema.passwordResets)
+    .values({
+      token,
+      expiresAt,
+      userId: user.id,
+
+    })
+    .returning()
+
+  if (!createdReset) throw ServerError('errors.auth.forgotPassword.failed')
+  const resetUrl = withBaseUrl(withQuery('/auth/reset-password', { token }))
+
+  if (import.meta.dev) {
+    console.log(`Reset password with this URL: ${resetUrl}`)
+    return {
+      ok: true,
+      expiresAt,
+      local: true,
+    }
+  }
+
+  await sendEmailTemplate(event, {
+    to: user.email,
+    from,
+    replyTo,
+    subject: 'Password Reset',
+    component: PasswordResetEmail,
+    props: {
+      resetUrl,
+    },
+  })
+
+  return { ok: true }
 })
