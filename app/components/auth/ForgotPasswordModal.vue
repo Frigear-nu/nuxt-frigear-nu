6c8057babee6ea16bfbf220903347b944d775cee
hareland <packagist@proton.me>
2026-01-21 05:12:01 +0100

    refactor: enhance ForgotPasswordModal with form validation, i18n, and improved submission handling

--- app/components/auth/ForgotPasswordModal.vue
+++ app/components/auth/ForgotPasswordModal.vue
@@ -1,17 +1,91 @@
 <script lang="ts" setup>
+import { useAuth } from '#imports'
+import type { ForgotPasswordSchema } from '#shared/schema/auth'
+import { forgotPasswordSchema } from '#shared/schema/auth'
+import type { FormSubmitEvent } from '@nuxt/ui'
+import type { AuthError } from '@supabase/auth-js'
+import type { ZodError } from 'zod'
+
+const $emits = defineEmits<{
+  (e: 'loading'): void
+  (e: 'success', email: string): void
+  (e: 'error', error: Error | AuthError | z.ZodError): void
+  (e: 'development', magicLink: unknown): void
+}>()
+
 const displayModal = defineModel<boolean>('open', { default: false })
+const emailValue = defineModel<string | undefined>('email')
+
+const { sendForgotPasswordEmail } = useAuth()
+const form = useTemplateRef('form')
+
+const DEFAULT_STATE: Partial<ForgotPasswordSchema> = {
+  email: undefined,
+}
+
+const state = reactive<typeof DEFAULT_STATE>({ ...DEFAULT_STATE })
+
+watch(emailValue, (em) => {
+  state.email = em
+})
+
+async function onSubmit(payload: FormSubmitEvent<ForgotPasswordSchema>) {
+  const email = payload.data.email
+  try {
+    const forgotPassword = await sendForgotPasswordEmail(email)
+    if (import.meta.dev) console.log({ forgotPassword })
+
+    if (typeof forgotPassword === 'object' && forgotPassword?.local) {
+      return $emits('development', forgotPassword)
+    }
+
+    $emits('success', email)
+  }
+  catch (error: unknown) {
+    $emits('error', error as Error | AuthError | ZodError)
+  }
+}
 </script>
 
 <template>
   <UModal
     v-model:open="displayModal"
-    title="Forgot password?"
+    :title="$t('auth.forgotPassword.title')"
   >
     <template #body>
-      So... you forgot your password, again...?
+      <UForm
+        ref="form"
+        :schema="forgotPasswordSchema"
+        :state="state"
+        class="space-y-4"
+        @submit="onSubmit"
+      >
+        <UFormField
+          :label="$t('auth.email.label')"
+          name="email"
+        >
+          <UInput
+            v-model="state.email"
+            :placeholder="$t('auth.email.placeholder')"
+          />
+        </UFormField>
+      </UForm>
     </template>
     <template #footer>
-      TODO...
+      <div class="flex justify-end w-full gap-2">
+        <UButton
+          :label="$t('actions.cancel')"
+          trailing-icon="i-lucide-x"
+        />
+        <UButton
+          v-if="form"
+          :label="$t('auth.forgotPassword.submit')"
+          trailing-icon="i-lucide-arrow-right"
+          type="submit"
+          :loading="form.loading"
+          @click="form.submit()"
+        />
+      </div>
     </template>
   </UModal>
 </template>
