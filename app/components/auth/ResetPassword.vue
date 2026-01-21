6a6aa7b221464df8aab210f1036c39af31f936e2
hareland <packagist@proton.me>
2026-01-21 05:12:28 +0100

    feat: reset password

---
+++ app/components/auth/ResetPassword.vue
@@ -0,0 +1,86 @@
+<script lang="ts" setup>
+import { useUrlSearchParams } from '@vueuse/core'
+import { type ResetPasswordSchema, resetPasswordSchema } from '#shared/schema/auth'
+import type { FormSubmitEvent } from '@nuxt/ui'
+import { useAuth, useToast, useSiteI18n } from '#imports'
+
+const searchParams = useUrlSearchParams<{ token?: string }>('history')
+onMounted(() => {
+  if (!searchParams.token) {
+    throw createError({
+      statusCode: 404,
+    })
+  }
+})
+
+const { resetPassword, refresh } = useAuth()
+const { localePath } = useSiteI18n()
+
+const form = useTemplateRef('form')
+const toast = useToast()
+const state = reactive<Partial<ResetPasswordSchema>>({
+  token: undefined,
+  password: undefined,
+  confirmPassword: undefined,
+})
+
+watchEffect(() => {
+  if (searchParams.token) state.token = searchParams.token
+})
+
+async function onSubmit(payload: FormSubmitEvent<ResetPasswordSchema>) {
+  const { token, password, confirmPassword } = payload.data
+  try {
+    await resetPassword(token, password, confirmPassword)
+    await refresh()
+    toast.add(formatToastSuccess('Password has been set.'))
+    await navigateTo(localePath('/account'))
+  }
+  catch (err: unknown) {
+    toast.add(formatToastError(err as never))
+  }
+}
+</script>
+
+<template>
+  <UForm
+    id="reset-password"
+    ref="form"
+    :schema="resetPasswordSchema"
+    :state="state"
+    class="w-full max-w-md"
+    @submit="onSubmit"
+  >
+    <UPageCard
+      title="Reset password"
+      variant="subtle"
+      class="space-y-4"
+    >
+      <UFormField label="Password">
+        <UInput
+          v-model="state.password"
+          type="password"
+          autocomplete="new-password"
+        />
+      </UFormField>
+      <UFormField label="Confirm Password">
+        <UInput
+          v-model="state.confirmPassword"
+          type="password"
+          autocomplete="new-password"
+        />
+      </UFormField>
+      <div class="flex justify-end">
+        <UButton
+          v-if="form"
+          type="submit"
+          trailing-icon="i-lucide-check"
+          :loading="form.loading"
+          @click="form.submit()"
+        >
+          Reset password
+        </UButton>
+      </div>
+    </UPageCard>
+  </UForm>
+</template>
