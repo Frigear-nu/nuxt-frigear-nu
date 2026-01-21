b1912e2aed6ffe06202dc2a3117642ad7a0ecccc
hareland <packagist@proton.me>
2026-01-21 18:46:53 +0100

    feat: replace AppHeader user button with AccountMenu component

--- app/components/account/AccountMenu.vue
+++ app/components/account/AccountMenu.vue
@@ -1,39 +1,59 @@
 <script setup lang="ts">
+import type { ButtonProps } from '@nuxt/ui'
+import type { NuxtLinkProps } from '#app'
+import { useAuth, useSiteI18n } from '#imports'
 
+const { t, localePath } = useSiteI18n()
+const { signOut } = useAuth()
+
+const onSignOut = async () => {
+  await signOut()
+  await navigateTo(localePath('/'))
+}
+
+const items = computed<ButtonProps[]>(() => ([
+  {
+    label: t('auth.dashboard'),
+    to: localePath('/account'),
+    icon: 'i-lucide-layout-dashboard',
+  },
+  {
+    label: t('auth.signOut'),
+    icon: 'i-lucide-log-out',
+    to: '#',
+    onClick: onSignOut,
+  },
+]))
 </script>
 
 <template>
   <UPopover
+    :mode="$device.isMobile ? 'click' : 'hover'"
     :content="{ align: 'end' }"
   >
     <UButton
       color="neutral"
       variant="ghost"
       class="size-8"
-    >
-      <template #trailing>
-        <span class="text-lg">
-          {{ getEmojiFlag(locale) }}
-        </span>
-      </template>
-    </UButton>
+      icon="i-lucide-user"
+    />
 
     <template #content>
       <ul class="flex flex-col">
         <li
-          v-for="localeItem in locales"
-          :key="localeItem.code"
+          v-for="linkItem in items"
+          :key="linkItem.label"
         >
           <NuxtLink
             class="flex justify-between py-1.5 px-2 gap-1 hover:bg-muted"
-            :to="switchLocalePath(localeItem.code) as string"
-            :aria-label="localeItem.name"
+            :aria-label="linkItem.label"
+            v-bind="linkItem as NuxtLinkProps"
           >
             <span class="text-sm">
-              {{ localeItem.name }}
+              {{ linkItem.label }}
             </span>
             <span class="size-5 text-center">
-              {{ getEmojiFlag(localeItem.code) }}
+              <UIcon :name="linkItem.icon || 'i-lucide-x'" />
             </span>
           </NuxtLink>
         </li>
