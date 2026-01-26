97ef3f04a7abe0c07d553775a29255275f5a6b58
hareland <packagist@proton.me>
2026-01-26 16:02:43 +0100

    refactor: #96 remove unused code

--- server/utils/stripe.ts
+++ server/utils/stripe.ts
@@ -1,5 +1,4 @@
 import type { Users } from 'hub:db:schema'
-import { eq } from 'drizzle-orm'
 
 export const createStripeCustomerFromMigration = async (user: Users) => {
   if (user.isMigrated) return
@@ -18,25 +17,3 @@ export const createStripeCustomerFromMigration = async (user: Users) => {
     passwordHash: null,
   }).where(eq(schema.users.id, user.id))
 }
-
-// export const syncStripeSubscriptionsForCustomer = async (event: H3Event, customerId: string) => {
-// const stripe = useServerStripe(event)
-// const { data: subscriptions } = await stripe.subscriptions.list({
-//   customer: customerId,
-// })
-
-// // @ts-expect-error There is some typing issue with drizzle for now
-// db.insert(schema.stripeSubscriptions).values(subscriptions.map((s) => {
-//   return <NewStripeCustomers>{
-//     id: s.id as unknown as string,
-//     userId: customerId as number | string,
-//     status: s.status as string,
-//     metadata: s.metadata as Record<string, string>,
-//     quantity: 1,
-//     cancelAtPeriodEnd: s.cancel_at_period_end,
-//     created: new Date(s.created),
-//     currentPeriodStart: new Date(s.start_date),
-//     endedAt: s.ended_at ? new Date(s.ended_at) : null,
-//   }
-// }))
-// }
