8fef8a437f768b5e56be856f6b36723744893678
hareland <packagist@proton.me>
2026-01-27 02:43:30 +0100

    refactor: introduce `useTaskStripe` helper for Stripe client initialization

--- server/utils/stripe.ts
+++ server/utils/stripe.ts
@@ -1,4 +1,13 @@
 import type { Users } from 'hub:db:schema'
+import { eq } from 'drizzle-orm'
+import Stripe from 'stripe'
+
+// This is only required due to the stripe module requiring access to the event context.
+export const useTaskStripe = () => {
+  const { stripe: { key: stripeKey } } = useRuntimeConfig()
+
+  return new Stripe(stripeKey)
+}
 
 export const createStripeCustomerFromMigration = async (user: Users) => {
   if (user.isMigrated) return
