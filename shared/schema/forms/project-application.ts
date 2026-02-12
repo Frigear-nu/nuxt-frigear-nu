ea3de860a267f209672f134d70bedbe2334652b2
hareland <packagist@proton.me>
2026-02-12 18:43:17 +0100

    feat(forms): refactor project application form to use schemas instead of fields

--- shared/schema/forms/project-application.ts
+++ shared/schema/forms/project-application.ts
@@ -1,82 +1,69 @@
-import { z, type ZodSchema } from 'zod'
-
-type FormField = {
-  name: string
-  labelKey?: string
-  placeholderKey?: string
-  hintKey?: string
-  type: 'textarea' | 'input' | 'number' | 'date' | 'switch' | 'files'
-  defaultValue?: string | number | boolean
-  if?: string
-}
+import { z, type ZodType } from 'zod/v4'
 
 type FormSection = {
   id: string
   labelKey?: string
   descriptionKey?: string
-  fields: FormField[]
+  schema: ZodType
 }
 
-type Form = {
-  schema: ZodSchema
+// TODO: Could possibly be flattened into one object instead of ID
+export type SteppedForm = {
+  id: string
   sections: FormSection[]
 }
 
-export const projectApplicationForm: Form = {
-  schema: z.object({
-    background: z.string(),
-    purpose: z.string(),
-    participants: z.coerce.number(),
-    when: z.date(),
-    // TODO..
-  }),
+export const projectApplicationForm: SteppedForm = {
+  id: 'project-application',
   sections: [
     {
-      id: 'about',
-      fields: [
-        { name: 'background', type: 'textarea' },
-        { name: 'purpose', type: 'textarea' },
-      ],
+      id: 'background',
+      schema: z.object({
+        // NOTE: We want to use i18n keys where possible, so we might have customized autoform...
+        background: z.string().meta({ title: 'form.application.background', type: 'textarea' }),
+        purpose: z.string().meta({ title: 'form.application.purpose', type: 'textarea' }),
+      }),
     },
     {
       id: 'project',
-      fields: [
-        { name: 'participants', type: 'number' },
-        { name: 'when', type: 'date' },
-        { name: 'projectSupportedByOthers', type: 'switch', defaultValue: false },
-        { name: 'supportedByOther', type: 'textarea', if: 'projectSupportedByOthers' },
-      ],
+      schema: z.object({
+        participants: z.number(),
+        when: z.date(),
+        isSupportedByOthers: z.boolean().default(false),
+        supportedByOthers: z.string().optional(),
+      }),
     },
     {
       id: 'budget',
-      fields: [
-        { name: 'totalBudget', type: 'number' },
-        { name: 'fundsUsage', type: 'textarea' },
-        { name: 'ownDeductible', type: 'number' },
-        { name: 'profit', type: 'number' },
-        { name: 'profitPurpose', type: 'textarea' },
-      ],
+      schema: z.object({
+        totalBudget: z.number(),
+        fundsUsage: z.string(),
+        ownDeductible: z.string(),
+        profit: z.number().optional(),
+        profitPurpose: z.string(),
+      }),
     },
     {
       id: 'legal',
-      fields: [
-        { name: 'projectName', type: 'input' },
-        { name: 'projectResponsible', type: 'textarea' },
-        { name: 'cvrCpr', type: 'textarea' },
-      ],
+
+      schema: z.object({
+        projectName: z.string(),
+        projectResponsible: z.string(),
+        cvrCpr: z.string(),
+      }),
     },
     {
       id: 'info',
-      fields: [
-        { name: 'websiteSoMe', type: 'textarea' },
-        { name: 'paymentDetails', type: 'textarea' },
-      ],
+      schema: z.object({
+        websiteSoMe: z.string(),
+        paymentDetails: z.string(),
+      }),
     },
     {
       id: 'attachments',
-      fields: [
-        { name: 'attachments', type: 'files' },
-      ],
+      schema: z.object({
+        attachments: z.array(z.file()),
+      }),
     },
   ],
 }
