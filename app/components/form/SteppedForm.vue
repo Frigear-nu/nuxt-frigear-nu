bba5192936479b7237cb7e26f187d83a7e0e4ed3
hareland <packagist@proton.me>
2026-02-12 18:43:06 +0100

    feat(forms): add SteppedForm component for project application

---
+++ app/components/form/SteppedForm.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+// FIXME: Move to separate file.
+import type { SteppedForm } from '#shared/schema/forms/project-application'
+
+defineProps<{
+  form: SteppedForm
+}>()
+
+// TODO: Might want to clone the repo and customize the module ourselves.
+</script>
+
+<template>
+  <div>
+    <pre>{{ form }}</pre>
+  </div>
+</template>
