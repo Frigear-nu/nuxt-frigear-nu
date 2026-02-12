86e5d2646826e9481b2693ef0116ad0092578279
hareland <packagist@proton.me>
2026-02-12 18:11:43 +0100

    feat(forms): add schema and structure for project application form

---
+++ shared/schema/forms/project-application.ts
@@ -0,0 +1,80 @@
+import { z, type ZodSchema } from 'zod'
+
+type FormField = {
+  name: string
+  labelKey?: string
+  placeholderKey?: string
+  hintKey?: string
+  type: 'textarea' | 'input' | 'number' | 'date' | 'switch' | 'files'
+  if?: string
+}
+
+type FormSection = {
+  id: string
+  labelKey?: string
+  descriptionKey?: string
+  fields: FormField[]
+}
+
+type Form = {
+  schema: ZodSchema
+  sections: FormSection[]
+}
+
+export const projectApplicationForm: Form = {
+  schema: z.object({
+    background: z.string(),
+    purpose: z.string(),
+    participants: z.coerce.number(),
+    when: z.date(),
+  }),
+  sections: [
+    {
+      id: 'about',
+      fields: [
+        { name: 'background', type: 'textarea' },
+        { name: 'purpose', type: 'textarea' },
+      ],
+    },
+    {
+      id: 'project',
+      fields: [
+        { name: 'participants', type: 'number' },
+        { name: 'when', type: 'date' },
+        { name: 'projectSupportedByOthers', type: 'switch' },
+        { name: 'supportedByOther', type: 'textarea', if: 'projectSupportedByOthers' },
+      ],
+    },
+    {
+      id: 'budget',
+      fields: [
+        { name: 'totalBudget', type: 'number' },
+        { name: 'fundsUsage', type: 'textarea' },
+        { name: 'ownDeductible', type: 'number' },
+        { name: 'profit', type: 'number' },
+        { name: 'profitPurpose', type: 'textarea' },
+      ],
+    },
+    {
+      id: 'legal',
+      fields: [
+        { name: 'projectName', type: 'input' },
+        { name: 'projectResponsible', type: 'textarea' },
+        { name: 'cvrCpr', type: 'textarea' },
+      ],
+    },
+    {
+      id: 'info',
+      fields: [
+        { name: 'websiteSoMe', type: 'textarea' },
+        { name: 'paymentDetails', type: 'textarea' },
+      ],
+    },
+    {
+      id: 'attachments',
+      fields: [
+        { name: 'attachments', type: 'files' },
+      ],
+    },
+  ],
+}
