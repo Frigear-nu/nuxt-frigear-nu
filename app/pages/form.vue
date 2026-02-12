9204316f64057272e8d0aa3701e9b0f4f56dbfb6
hareland <packagist@proton.me>
2026-02-12 18:43:10 +0100

    feat(forms): add form page for project application

---
+++ app/pages/form.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { projectApplicationForm } from '#shared/schema/forms/project-application'
+</script>
+
+<template>
+  <FormSteppedForm :form="projectApplicationForm" />
+</template>
