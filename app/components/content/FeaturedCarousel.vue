e43a25b60c58eb4f4e498bc32f04fac2e4560869
hareland <32701939+hareland@users.noreply.github.com>
2026-02-23 13:52:17 +0100

    feat: add component

---
+++ app/components/content/FeaturedCarousel.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import ListCarousel from '~/components/ListCarousel.vue'
+
+const featuredProducts = ref([
+  {
+    title: 'One',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, quia.',
+    tags: ['nuxt', 'vue', 'typescript'],
+  },
+  {
+    title: 'Two',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, quia.',
+    tags: ['nuxt', 'vue', 'typescript'],
+    image: 'https://madewithnetworkfra.fra1.digitaloceanspaces.com/spatie-space-production/31972/vuetify-snips.jpg',
+  },
+  {
+    title: 'Three',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, quia.',
+    tags: ['nuxt', 'vue', 'typescript'],
+  },
+])
+</script>
+
+<template>
+      <ListCarousel :items="featuredProducts">
+        <template #tag="{ tag, isCurrent }">
+          <NuxtLink :to="`/tags/${tag}`">
+            <UBadge
+              :color="isCurrent ? 'primary' : 'secondary'"
+              :variant="isCurrent ? 'solid' : 'outline'"
+            >
+              #{{ tag }}
+            </UBadge>
+          </NuxtLink>
+        </template>
+      </ListCarousel>
+</template>
