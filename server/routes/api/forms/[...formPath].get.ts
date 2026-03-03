a3f4105adc12615eb17c0d475a19be2a46094bda
hareland <packagist@proton.me>
2026-03-03 17:24:32 +0100

    chore(refactor): replace generic 404 error with NotFoundError in form API route

--- server/routes/api/forms/[...formPath].get.ts
+++ server/routes/api/forms/[...formPath].get.ts
@@ -1,17 +1,18 @@
 // for future usage...
 import { withLeadingSlash } from 'ufo'
 import { objectOmit } from '@vueuse/core'
+import { NotFoundError } from '@nitrotool/errors'
 
 export default defineEventHandler(async (event) => {
   const formPath = getRouterParam(event, 'formPath')
 
-  if (!formPath) throw createError({ statusCode: 404 })
+  if (!formPath) throw NotFoundError()
 
   const form = await queryCollection(event, 'forms')
     .path(withLeadingSlash(formPath))
     .first()
 
-  if (!form) throw createError({ statusCode: 404 })
+  if (!form) throw NotFoundError()
 
   return objectOmit(form, ['delivery'])
 })
