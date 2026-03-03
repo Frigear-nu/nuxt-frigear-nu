640150dd14a182bda3fe20bc18e9c0f4c1634503
hareland <packagist@proton.me>
2026-03-03 22:27:39 +0100

    chore(refactor): replace `<NuxtPage />` with `<slot />` in form layout for improved flexibility

--- app/layouts/form.vue
+++ app/layouts/form.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-  <div class="h-[calc(100vh-var(--ui-header-height))]  bg-neutral-50 dark:bg-neutral-950 flex items-center justify-center p-4">
+  <div class="h-[calc(100vh-var(--ui-header-height))] bg-neutral-50 dark:bg-neutral-950 flex items-center justify-center p-4">
     <div class="w-full max-w-md">
-      <NuxtPage />
+      <slot />
     </div>
   </div>
 </template>
