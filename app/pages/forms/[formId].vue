831e311f244b43783cbc12ead0a27a5228225e29
hareland <packagist@proton.me>
2026-03-04 19:51:21 +0100

    feat(forms): card bg

--- app/pages/forms/[formId].vue
+++ app/pages/forms/[formId].vue
@@ -114,7 +114,10 @@ const onComplete = async (args: ProjectApplicationForm) => {
         {{ $t(form.description) }}
       </div>
     </div>
-    <UCard>
+    <UCard
+      variant="subtle"
+      class="dark:bg-neutral-950"
+    >
       <div
         v-if="steppedForm?.steps && steppedForm.steps.length > 1 && !wasSubmitted"
         class="flex justify-center gap-1 mb-8"
