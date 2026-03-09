680d70619a7570685e48c3f59db829f1554fe701
hareland <packagist@proton.me>
2026-03-09 12:37:39 +0100

    fix(i18n): update `i18n-preload` module to dynamically configure prerender routes based on locales

--- modules/preload-i18n-pages.ts
+++ modules/preload-i18n-pages.ts
@@ -1,4 +1,5 @@
-import { defineNuxtModule /* ,useNuxt */ } from '@nuxt/kit'
+import { defineNuxtModule, useNuxt } from '@nuxt/kit'
+import { withLeadingSlash, withTrailingSlash, joinRelativeURL } from 'ufo'
 
 export default defineNuxtModule({
   meta: {
@@ -6,26 +7,43 @@ export default defineNuxtModule({
     configKey: 'i18nPreload',
   },
   hooks: {
-    'nitro:config'(
-      // nitroConfig
-    ) {
+    'nitro:config'(nitroConfig) {
       // TODO: add the path of a locale prefixed path to the prerender list.
-      // const nuxt = useNuxt()
+      const nuxt = useNuxt()
+      const i18nOptions = nuxt.options.i18n
+      const defaultLocale = i18nOptions.defaultLocale
+      nitroConfig.prerender = nitroConfig.prerender || {}
+      nitroConfig.prerender.routes = nitroConfig.prerender.routes || []
       //
-      // const i18nOptions = nuxt.options.i18n
-      //
-      // const routes: string[] = []
-      // if (!i18nOptions) {
-      //   routes.push('/')
-      // }
-      // else {
-      //   routes.push(...(i18nOptions.locales?.map(locale => typeof locale === 'string' ? `/${locale}` : `/${locale.code}`) || []))
-      // }
-      //
-      // nitroConfig.prerender = nitroConfig.prerender || {}
-      // nitroConfig.prerender.routes = nitroConfig.prerender.routes || []
-      // nitroConfig.prerender.routes.push(...(routes || []))
-      /// / nitroConfig.prerender.routes.push('/sitemap.xml')
+      const routes: string[] = []
+      if (i18nOptions) {
+        const localesToPrerender = i18nOptions.locales?.map((locale) => {
+          return typeof locale === 'string' ? locale : locale.code
+        })
+
+        if (localesToPrerender) {
+          for (const route in nitroConfig.prerender.routes) {
+            if (!route) continue
+
+            // Find all items without a locale prefix,
+            // and add them to the prerender list with the non-default locale prefixes
+            routes.push(
+              ...localesToPrerender
+                .filter((locale) => {
+                  return locale !== defaultLocale
+                  // Ensures that we can pre-render specific locale pages too.
+                    && !route.startsWith(withLeadingSlash(withTrailingSlash(locale)))
+                })
+                .map((locale) => {
+                  return withLeadingSlash(joinRelativeURL(locale, route))
+                }),
+            )
+          }
+        }
+      }
+
+      nitroConfig.prerender.routes.push(...(routes || []))
+      // nitroConfig.prerender.routes.push('/sitemap.xml')
     },
   },
 })
