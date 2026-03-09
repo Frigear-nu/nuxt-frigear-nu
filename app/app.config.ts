c5b9516f2279ffc447f12b5eedd39e3511ba0566
51X415 <blockstarterinfo@gmail.com>
2026-03-09 17:44:59 +0100

    Fixed: h1s overflowing pages, -cards too much padding and margin on mmobile

--- app/app.config.ts
+++ app/app.config.ts
@@ -3,6 +3,15 @@ export default defineAppConfig({
     name: 'Frigear.nu',
   },
   ui: {
+    section: {
+      container: 'w-full max-w-(--ui-container) mx-auto px-0 sm:px-3 lg:px-6 flex flex-col lg:grid py-8 sm:py-16 lg:py-24 gap-6 sm:gap-12',
+    },
+    pageHero: {
+      container: 'w-full max-w-(--ui-container) mx-auto px-2 sm:px-4 lg:px-6 flex flex-col lg:grid py-12 sm:py-16 lg:py-24 gap-12 sm:gap-y-10',
+      slots: {
+        title: 'text-4xl sm:text-7xl text-pretty tracking-tight font-bold text-highlighted',
+      },
+    },
     colors: {
       primary: 'violet',
       neutral: 'neutral',
