e5380160fc9182f64c90cfa98c74b4d46b2248f2
51X415 <blockstarterinfo@gmail.com>
2026-03-12 15:05:36 +0100

    Made application-form resposive on sm:+

--- app/layouts/form.vue
+++ app/layouts/form.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="h-[calc(100vh-var(--ui-header-height))] flex items-center justify-center p-4">
-    <div class="w-full max-w-md">
+    <div class="w-full sm:max-w-5/6 md:max-w-4/6 lg:max-w-3/6 xl:max-w-2/6">
       <slot />
     </div>
   </div>
