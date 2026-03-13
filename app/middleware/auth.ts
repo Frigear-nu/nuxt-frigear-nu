181544b7a500849fc25cd9e64d513cf566cd9788
hareland <packagist@proton.me>
2026-03-13 03:58:59 +0100

    fix(auth): refine route middleware to validate admin group access

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,11 +1,16 @@
 import { useUserSession } from '#imports'
 
 export default defineNuxtRouteMiddleware((to) => {
-  if (!to.path.startsWith('/account')) {
-    return
-  }
+  const isAccountRoute = to.path.startsWith('/account')
+  const isAdminGroup = to.meta.groups?.includes('admin') ?? true
+
+  if (!isAccountRoute || !isAdminGroup) return
+
   const { loggedIn } = useUserSession()
 
+  if (isAdminGroup) {
+    // todo: check admin rights..
+  }
   if (loggedIn.value) return
   return navigateTo(`/sign-in?redirect=${encodeURIComponent(to.fullPath)}`, { replace: true })
 })
