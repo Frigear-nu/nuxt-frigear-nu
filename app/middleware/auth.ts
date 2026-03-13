3a03eccde09ca16ca70749fa60a9f77daca08c94
hareland <packagist@proton.me>
2026-03-13 15:48:08 +0100

    fix(auth): refine route middleware to improve admin and account route handling

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -2,20 +2,25 @@ import { useUserSession } from '#imports'
 
 export default defineNuxtRouteMiddleware((to) => {
   const isAccountRoute = to.path.startsWith('/account')
-  const isAdminGroup = to.meta.groups?.includes('admin') ?? true
+  const isAdminRoute = to.meta.groups?.includes('admin') ?? false
 
-  if (!isAccountRoute || !isAdminGroup) return
+  // not relevant:
+  if (!isAccountRoute && !isAdminRoute) return
 
   const { user, loggedIn } = useUserSession()
 
-  if (!isAdminGroup && loggedIn.value) {
-    return
+  // TODO: We might want to configure the paths that require auth in nuxt.config.ts in the future
+  if (isAccountRoute && !loggedIn.value) {
+    return navigateTo(`/sign-in?redirect=${encodeURIComponent(to.fullPath)}`, { replace: true })
   }
 
-  if (isAdminGroup
-    && user.value?.email
-    && useRuntimeConfig()?.acl?.admins?.split(',').includes(user.value.email)) {
-    return
+  // TODO: Migrate "admin" flags to db in V1 via ACL task
+  if (isAdminRoute) {
+    const admins = useRuntimeConfig()?.acl?.admins?.split(',') ?? []
+    const isAdmin = loggedIn.value && !!user.value?.email && admins.includes(user.value.email)
+
+    if (!isAdmin) {
+      return navigateTo(`/sign-in?redirect=${encodeURIComponent(to.fullPath)}`, { replace: true })
+    }
   }
-  return navigateTo(`/sign-in?redirect=${encodeURIComponent(to.fullPath)}`, { replace: true })
 })
