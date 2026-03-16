4d74f53e424341aa1881de5fca7027f0d8cb8523
hareland <packagist@proton.me>
2026-03-16 02:02:00 +0100

    fix(sentry): add runtime config check for DSN and fallback for tracesSampleRate

--- server/plugins/001.sentry.ts
+++ server/plugins/001.sentry.ts
@@ -1,6 +1,13 @@
 import { sentryCloudflareNitroPlugin } from '@sentry/nuxt/module/plugins'
 
-export default defineNitroPlugin(sentryCloudflareNitroPlugin({
-  dsn: process.env.NUXT_PUBLIC_SENTRY_DSN,
-  tracesSampleRate: 1.0,
-}))
+export default defineNitroPlugin((nitroApp) => {
+  if (!useRuntimeConfig().public.sentry.dsn) {
+    console.warn('Sentry DSN not found, skipping Sentry plugin.')
+    return
+  }
+
+  return sentryCloudflareNitroPlugin({
+    dsn: useRuntimeConfig().public.sentry.dsn,
+    tracesSampleRate: useRuntimeConfig().public.sentry?.tracesSampleRate || 1.0,
+  })(nitroApp)
+})
