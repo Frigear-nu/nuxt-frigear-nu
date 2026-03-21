1e98657c6488bb721653ab7b63edf727f40fb9af
hareland <packagist@proton.me>
2026-03-21 14:19:07 +0100

    feat(admin): enhance submissions page with header links and preview functionality

--- app/pages/(admin)/admin/forms/[...formPath]/submissions/index.vue
+++ app/pages/(admin)/admin/forms/[...formPath]/submissions/index.vue
@@ -1,14 +1,68 @@
 <script setup lang="ts">
+import type { ButtonProps } from '@nuxt/ui'
 import useFormAsAdmin from '~/composables/admin/useFormAsAdmin'
+import { withLeadingSlash } from 'ufo'
 
-const { data: form } = await useFormAsAdmin()
+const route = useRoute()
+const { $api } = useNuxtApp()
+const [{ data: form }, { data: submissions }] = await Promise.all([
+  useFormAsAdmin(),
+  useAsyncData(() => `admin:form:${route.path}:submissions`, async () => {
+    return $api(`/api/admin/forms${withLeadingSlash(route.params.formPath.join('/'))}`)
+  }),
+])
+
+const headerTitle = computed(() => {
+  if (!form.value) {
+    return 'n/a'
+  }
+
+  return `${form.value.name} submissions`
+})
+
+const headerLinks = computed<ButtonProps[]>(() => [
+  {
+    label: 'Start a new submission',
+    to: `/forms${withLeadingSlash(form.value.path)}`,
+  },
+])
+
+const getPreview = (submission: typeof submissions.value[0]) => {
+  if (submission.data && typeof submission.data === 'object') {
+    const { files: _, ...data } = submission.data
+    return Object.values(data).map(v => typeof v === 'object' ? JSON.stringify(v) : v)
+      .slice(0, 3)
+  }
+
+  return []
+}
 </script>
 
 <template>
   <UContainer>
-    <UPageHeader title="Submissions" />
-    <UCard>
-      <pre>{{ form }}</pre>
-    </UCard>
+    <UPageHeader
+      :title="headerTitle"
+      :links="headerLinks"
+    />
+    <UPageList>
+      <UPageCard
+        v-for="submission in submissions"
+        :key="submission.id"
+        :title="submission.id"
+        :to="`/admin/forms${withLeadingSlash(form.path)}/submissions/${submission.id}`"
+      >
+        <template #description>
+          <b>Preview:</b><br>
+          <div class="flex flex-col gap-0">
+            <div
+              v-for="item in getPreview(submission)"
+              :key="item"
+            >
+              {{ item }}
+            </div>
+          </div>
+        </template>
+      </UPageCard>
+    </UPageList>
   </UContainer>
 </template>
