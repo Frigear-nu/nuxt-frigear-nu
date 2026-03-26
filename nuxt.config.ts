d5b6d64e37b5421fd0654e7876f4234912c8257a
hareland <packagist@proton.me>
2026-03-26 20:02:38 +0100

    fix(build): add empty `routes` array to prerender config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,6 +41,7 @@ export default defineNuxtConfig({
         ],
       },
       prerender: {
+        routes: [],
         ignore: ['/da', '/en'],
         failOnError: false, // todo: this should be investigated.
         crawlLinks: false,
