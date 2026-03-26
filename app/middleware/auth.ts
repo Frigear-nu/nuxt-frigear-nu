95d4853b14811ec3fbd5a9180b546a756e078d4e
hareland <packagist@proton.me>
2026-03-26 15:44:52 +0100

    fix(auth): update admin middleware to support dynamic permission checks

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,6 +1,8 @@
 import { useUserSession } from '#imports'
+import { allows } from 'nuxt-authorization/utils'
+import { canViewAdminArea } from '#shared/abilities/admin'
 
-export default defineNuxtRouteMiddleware((to) => {
+export default defineNuxtRouteMiddleware(async (to) => {
   const isAccountRoute = to.path.startsWith('/account')
   const isAdminRoute = to.meta.groups?.includes('admin') ?? false
 
@@ -10,17 +12,21 @@ export default defineNuxtRouteMiddleware((to) => {
   const { user, loggedIn } = useUserSession()
 
   // TODO: We might want to configure the paths that require auth in nuxt.config.ts in the future
-  if (isAccountRoute && !loggedIn.value) {
-    return navigateTo(`/sign-in?redirect=${encodeURIComponent(to.fullPath)}`, { replace: true })
+  if (isAccountRoute && loggedIn.value) {
+    return
   }
 
-  // TODO: Migrate "admin" flags to db in V1 via ACL task
   if (isAdminRoute) {
+    // TODO: Phase out static admin flags
     const admins = useRuntimeConfig()?.acl?.admins?.split(',') ?? []
     const isAdmin = loggedIn.value && !!user.value?.email && admins.includes(user.value.email)
-
-    if (!isAdmin) {
-      return navigateTo(`/sign-in?redirect=${encodeURIComponent(to.fullPath)}`, { replace: true })
+    const isAllowedToView = await allows(canViewAdminArea, user.value)
+    if (isAdmin || isAllowedToView) {
+      return
     }
   }
+  return navigateTo({
+    path: '/sign-in',
+    query: { redirect: to.path },
+  }, { replace: true })
 })
