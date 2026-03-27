8c561b4ad330e995a293b4324ea18c39da14fd68
hareland <packagist@proton.me>
2026-03-27 18:02:26 +0100

    fix(config): update `prerender` routes in `nuxt.config.ts`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,8 +41,7 @@ export default defineNuxtConfig({
         ],
       },
       prerender: {
-        routes: [],
-        ignore: ['/da', '/en'],
+        routes: ['/', '/en'],
         failOnError: false, // todo: this should be investigated.
         crawlLinks: false,
       },
