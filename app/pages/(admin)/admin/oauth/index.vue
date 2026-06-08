0ff74bb8919e165baad436aa682e90697a23887d
hareland <packagist@proton.me>
2026-06-08 21:00:48 +0200

    feat(admin): add OAuth management page with view, edit & delete functionality

---
+++ app/pages/(admin)/admin/oauth/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <UContainer>
+    <UPageHeader title="OAuth" />
+    <UPageBody>
+      Should be able to: view, edit & delete OAuth Client Applications.
+    </UPageBody>
+  </UContainer>
+</template>
