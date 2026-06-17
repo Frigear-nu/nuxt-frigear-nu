3def026ad8805173fc7385b914387078f85c6fe7
hareland <packagist@proton.me>
2026-06-17 15:21:54 +0200

    chore(db): add `user_roskilde_wristband` table and new fields to `users` and `oauth_clients` schema

---
+++ server/db/migrations/sqlite/0008_cooing_iron_fist.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `user_roskilde_wristband` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`added_by` integer,
+	`user_id` integer NOT NULL,
+	`year` integer,
+	`band_id` text,
+	`band_serial` text,
+	`created_at` integer NOT NULL,
+	`updated_at` integer,
+	FOREIGN KEY (`added_by`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE set null,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `urwidx` ON `user_roskilde_wristband` (`user_id`,`year`);--> statement-breakpoint
+CREATE UNIQUE INDEX `urwbidx` ON `user_roskilde_wristband` (`band_id`);--> statement-breakpoint
+CREATE UNIQUE INDEX `urwbsidx` ON `user_roskilde_wristband` (`band_serial`);--> statement-breakpoint
+ALTER TABLE `oauth_clients` ADD `description` text;--> statement-breakpoint
+ALTER TABLE `oauth_clients` ADD `icon` text;--> statement-breakpoint
+ALTER TABLE `oauth_clients` ADD `priority` integer DEFAULT 0 NOT NULL;--> statement-breakpoint
+ALTER TABLE `users` ADD `phone` text;--> statement-breakpoint
+ALTER TABLE `users` ADD `roskilde_people_id` text;--> statement-breakpoint
+CREATE UNIQUE INDEX `users_phone_unique` ON `users` (`phone`);--> statement-breakpoint
+CREATE UNIQUE INDEX `users_roskilde_people_id_unique` ON `users` (`roskilde_people_id`);
--- server/db/schema/user.ts
+++ server/db/schema/user.ts
@@ -1,4 +1,4 @@
-import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core'
+import { sqliteTable, text, integer, uniqueIndex } from 'drizzle-orm/sqlite-core'
 import { relations } from 'drizzle-orm'
 import { stripeCustomers } from './stripe'
 import { userEventTickets } from './event'
@@ -7,6 +7,7 @@ export const users = sqliteTable('users', {
   id: integer('id').primaryKey({ autoIncrement: true }),
   name: text('name').notNull(),
   email: text('email').notNull().unique(),
+  phone: text('phone').unique(),
   role: text('role', { enum: [
     'admin',
     'manager',
@@ -22,6 +23,7 @@ export const users = sqliteTable('users', {
   passwordHash: text('password_hash'),
   avatarUrl: text('avatar_url'),
 
+  roskildePeopleId: text('roskilde_people_id').unique(),
   // For migration from supabase (will be removed in the future)
   isMigrated: integer('is_migrated', { mode: 'boolean' }).notNull().default(false),
   supabaseId: text('supabase_id'),
@@ -45,30 +47,34 @@ export const usersRelations = relations(users, ({ many }) => ({
   oauthApps: many(oauthApps),
   passkeys: many(passkeys),
   tickets: many(userEventTickets),
-  roskildeIds: many(userRoskildeId, { relationName: 'roskilde_id_user' }),
-  createdRoskildeIds: many(userRoskildeId, { relationName: 'roskilde_id_addedBy' }),
+  roskildeWristbands: many(userRoskildeWristband, { relationName: 'roskilde_wristband_user' }),
+  createdRoskildeWristbands: many(userRoskildeWristband, { relationName: 'roskilde_wristband_addedBy' }),
 }))
 
-export const userRoskildeId = sqliteTable('user_roskilde_id', {
+export const userRoskildeWristband = sqliteTable('user_roskilde_wristband', {
   id: integer('id').primaryKey({ autoIncrement: true }),
   addedBy: integer('added_by').references(() => users.id, { onDelete: 'set null' }),
   userId: integer('user_id').notNull().references(() => users.id, { onDelete: 'cascade' }),
   year: integer().$defaultFn(() => (new Date()).getFullYear()),
   bandId: text('band_id'),
   bandSerial: text('band_serial'),
-  createdAt: integer({ mode: 'timestamp' }).notNull().$defaultFn(() => new Date()),
-  updatedAt: integer({ mode: 'timestamp' }).$onUpdateFn(() => new Date()),
-})
+  createdAt: integer('created_at', { mode: 'timestamp' }).notNull().$defaultFn(() => new Date()),
+  updatedAt: integer('updated_at', { mode: 'timestamp' }).$onUpdateFn(() => new Date()),
+}, t => [
+  uniqueIndex('urwidx').on(t.userId, t.year),
+  uniqueIndex('urwbidx').on(t.bandId),
+  uniqueIndex('urwbsidx').on(t.bandSerial),
+])
 
-export const userRoskildeIdRelations = relations(userRoskildeId, ({ one }) => ({
+export const userRoskildeIdRelations = relations(userRoskildeWristband, ({ one }) => ({
   user: one(users, {
-    relationName: 'roskilde_id_user',
-    fields: [userRoskildeId.userId],
+    relationName: 'roskilde_wristband_user',
+    fields: [userRoskildeWristband.userId],
     references: [users.id],
   }),
   addedBy: one(users, {
-    relationName: 'roskilde_id_addedBy',
-    fields: [userRoskildeId.addedBy],
+    relationName: 'roskilde_wristband_addedBy',
+    fields: [userRoskildeWristband.addedBy],
     references: [users.id],
   }),
 }))
