83dbc84005889144c497c8fc57b3cde58521a7b6
hareland <packagist@proton.me>
2026-06-26 23:21:39 +0200

    feat(account): add documents page with file downloads
    Introduced a new "Documents" page in the account section, allowing users to download files. Updated locale files to include translations for the feature and added navigation to the documents page in the account index.

---
+++ app/pages/account/documents.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+const { locale } = useSiteI18n()
+const documents = [
+  { path: '/assets/files/BA-Koordinatormøde-2503-2026.docx', name: 'BA Koordinatormøde 25.03.2026', nameEn: 'BA Coordinatormeeting 25.03.2026', icon: 'i-lucide-file' },
+  { path: '/assets/files/Frivillig håndbog.docx', name: 'Frivillig Håndbog', nameEn: 'Volunteer Handbook', icon: 'i-lucide-file' },
+]
+</script>
+
+<template>
+  <UPageGrid>
+    <UPageCard
+      v-for="document in documents"
+      :key="document.path"
+      :title="document.name"
+      :icon="document.icon"
+    >
+      <UButton
+        download
+        external
+        :to="document.path"
+        icon="i-lucide-download"
+      >
+        {{ locale === 'en' ? 'Download' : 'Hent fil' }}
+      </UButton>
+    </UPageCard>
+  </UPageGrid>
+</template>
